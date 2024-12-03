eaa4385abd5f3681fe1f58305166adb6c6510d90
Abdul Kabeer <138903878+ABabdulkabir@users.noreply.github.com>
2024-12-03 20:12:54 +0500

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter any number to calculate factorial for");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
+
+            Console.WriteLine("Result is {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
